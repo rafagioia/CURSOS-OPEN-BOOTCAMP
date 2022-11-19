575b331ffb03e5169c5719a8b4c11e27fc3a3822
Rafa Gioia <117834598+rafagioia@users.noreply.github.com>
2022-11-19 12:46:08 -0300

    Add files via upload

---
+++ 2) JAVASCRIPT/MODULO 2/ejercicio2.js
@@ -0,0 +1,14 @@
+
+const fecha = new Date("May 12 1987");
+console.log(fecha);
+
+const libro = {
+    titulo:"Harry Potter y el cáliz de fuego", 
+    autor: "J. K. Rowling", 
+    fecha: 2000, 
+    url: "https://www.ebay.com/itm/132255748926?epid=89105411&hash=item1ecb0e933e:g:tFwAAOSwKoRZYo9m&amdata=enc%3AAQAHAAAA8IAx2n7KZlJeIxVuNmkduxVWqYeY%2B0H%2BBT6GFXInl1HDhIryr7qK5MjBBOIo4kyd9g8jAcY8XMvdNNVLKfvJDGyCkrkW%2BztosYJaq2P8gW0pG51fyL%2B5usP6w0YwJJ0wX0Go9l%2Bpi9WPV5zh%2BYSkf2C02wkrPgv2W3GvoFFFHI7kisGobCkVF18GAhaWXhykIe0E6oiLxnvf%2B4%2FVAAHBpizLJY32a3CCDcmBUNuafgv%2BzP2tskIhm9uBjo%2FlVzt1k3vsNFkx780Amy3O8eT8fsABpIgLU7O1zldk5lE7B9C8QrBa3X4u5pODmyyz87vDdQ%3D%3D%7Ctkp%3ABFBMxqD2h5Jh"
+
+}
+
+var datos = ["Rafael", 35, true, fecha, libro];
+console.log(datos);
