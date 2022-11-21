3ff9da24c7dac6a116b086a2493b4f8b25e13e7f
Rafa Gioia <117834598+rafagioia@users.noreply.github.com>
2022-11-21 12:23:15 -0300

    Add files via upload

---
+++ 2) JAVASCRIPT/MODULO 4/ejercicio4.js
@@ -0,0 +1,49 @@
+
+let nombre = "Rafael"
+console.log(nombre)
+let apellido = "Gioia"
+console.log(apellido)
+let estudiante = nombre.concat(" ", apellido)
+console.log(estudiante)
+let estudianteMayus = estudiante.toUpperCase()
+console.log(estudianteMayus)
+let estudianteMinus = estudiante.toLowerCase()
+console.log(estudianteMinus)
+let contarestudiante = estudiante.length
+console.log(contarestudiante)
+let letra_nombre = nombre.charAt(0)
+console.log(letra_nombre)
+let letra_apellido = apellido.charAt(apellido.length-1)
+console.log(letra_apellido)
+let estudiante_sinesp = estudiante.trim()
+console.log(estudiante_sinesp)
+let verif_nombre = estudiante.includes("Rafael")
+console.log(verif_nombre)
+
+
+
+
+
+
+
+// Crea un archivo JS que contenga las siguientes líneas
+
+// - Una cadena de texto con tu Nombre
+
+// - Otra cadena de texto con tu Apellido
+
+// - Una cadena de texto que se llame "estudiante" concatenando tu Nombre y tu Apellido con un espacio entre medias
+
+// - Una cadena de texto que se llame "estudianteMayus" que contenga la cadena estudiante pero todo en mayúsculas
+
+// - Una cadena de texto que se llame "estudianteMinus" que contenga la cadena estudiante pero todo en minúsculas
+
+// - Una variable que contenga el número de letras de la cadena "estudiante" contando los espacios
+
+// - Una variable que contenga la primera letra del Nombre
+
+// - Otra variable que contenga la última letra del Apellido
+
+// - Una cadena de texto que elimine los espacios de la variable "estudiante"
+
+// - Una variable booleana que diga si el Nombre está contenido en la variable "estudiante"
