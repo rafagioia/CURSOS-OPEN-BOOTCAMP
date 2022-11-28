1690c24dce1a8fd4d5e70723deb053f97ce92323
Rafa Gioia <117834598+rafagioia@users.noreply.github.com>
2022-11-28 12:07:56 -0300

    Add files via upload

---
+++ 2) JAVASCRIPT/MODULO 11/ejercicio11.js
@@ -0,0 +1,16 @@
+class Estudiante {
+    nombre = "Rafa"
+    asignaturas = ["Javascript","HTML","CSS"]
+    obtenDatos() {
+        return {
+        nombre: this.nombre,
+        asignaturas: this.asignaturas
+        }
+    }
+    
+}
+
+const estudiante = new Estudiante()
+
+console.log(estudiante.obtenDatos())
+
