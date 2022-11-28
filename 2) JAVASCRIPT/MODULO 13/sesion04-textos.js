491b768b5a16df85778ce13a218bd223df7cab79
Rafa Gioia <117834598+rafagioia@users.noreply.github.com>
2022-11-28 17:31:55 -0300

    Add files via upload

---
+++ 2) JAVASCRIPT/MODULO 13/sesion04-textos.js
@@ -0,0 +1,24 @@
+
+
+let saludo_comillas_dobles = "Hola"
+let saludo_comillas_simples = "Hola"
+let mezcladas_dobles = "Hola 'como estas?'"
+let mezcladas_simples = "Hola \"como estas?\""
+let barra_dobles = "Hola \"como estas?\""
+let barra_simples = "Hola \"como estas?\""
+
+let str_sng = "hola, este es un string"
+let str_dbl = "hola, este es un string"
+
+console.log(str_dbl)
+console.log(str_sng)
+
+let str_comillas = "hola prueba \"de comillas\""
+let str_comillassimples = "hola prueba de \"comillas simples\""
+let str_comillasdobles = "hola prueba de 'comillas simples'"
+let str_backticks = "Hola esta es una prueba de backticks"
+
+console.log(str_comillas)
+console.log(str_comillassimples)
+console.log(str_comillasdobles)
+
